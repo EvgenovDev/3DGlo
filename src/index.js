c19b21846db24dd9d236497f391a9bd7a317f1fb
Roman Evgenov <Roman Evgenov>
2021-10-29 04:32:10 +0300

    Основное готово

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import timer from "./modules/timer";
 import menu from "./modules/menu";
+import validate from "./modules/validate";
 
 timer("30 october 2021");
 menu();
+validate();
---
+++ src/modules/validate.js
@@ -0,0 +1,29 @@
+const validate = () => {
+ const calcValidate = () => {
+	const inputs = document.querySelectorAll(".calc-block>input");
+
+	inputs.forEach((elem) => {
+		elem.setAttribute("type", "number");
+		elem.addEventListener("input", () => {
+			elem.value = elem.value.replace(/\D/g, "");
+		});
+	});
+ };
+
+ const formValidate = (form) => {
+	form.querySelector("input:first-child").addEventListener("input", () => {
+		form.querySelector("input[placeholder='Ваше имя']").value = form.querySelector("input[placeholder='Ваше имя']").value.replace(/[^а-я\s\-]/gi, "");
+	});
+	form.querySelector("input[type='email']").addEventListener("input", () => {
+		form.querySelector("input[type='email']").value = form.querySelector("input[type='email']").value.replace(/[^a-z\-\@\_\.\!\~\*\']/gi, "");
+	});
+	form.querySelector("input[type='tel']").addEventListener("input", () => {
+		form.querySelector("input[type='tel']").value = form.querySelector("input[type='tel']").value.replace(/[^\d\(\)\-]/gi, "");
+	});
+ };
+
+ calcValidate();
+ formValidate(document.querySelector(".main-form"));
+};
+
+export default validate;
