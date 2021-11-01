6aed5bfe517da6f0dad880fc5824c4feab93499c
Roman Evgenov <Roman Evgenov>
2021-11-01 14:14:05 +0400

    Добавил слайдер

--- src/modules/sliderSwiper.js
+++ src/modules/sliderSwiper.js
@@ -1,8 +1,21 @@
 import Slider from "swiper";
 
 const sliderSwiper = () => {
-	const  slider = new Slider(".swiper", {
-		
+	const slider = new Slider(".swiper", {
+		slidesPerView: 1,
+		loop: true,
+		autoplay: {
+			delay: 2500,
+			disableOnInteraction: false
+		},
+		breakpoints: {
+			768: {
+			  slidesPerView: 2,
+			},
+			1250: {
+			  slidesPerView: 3,
+			},
+		 },
 	});
 };
 
