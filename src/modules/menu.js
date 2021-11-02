73d721749d0295bb669425cd860683d1716be713
Roman Evgenov <Roman Evgenov>
2021-11-02 13:32:44 +0400

    Усложненное готово

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -3,10 +3,25 @@ import scrolls from "./scrolls";
 const menu = () => {
 	const menu = document.querySelector("menu");
 	const serviceBlock = document.querySelector('a[href="#service-block"]');
-	const main = document.querySelector("main");
 
 	const toggleMenu = () => {
-		menu.classList.toggle("active-menu");
+		document.body.addEventListener("click", (e) => {
+			if (e.target.closest(".menu")) {
+				menu.classList.toggle("active-menu");
+			} else if (e.target === serviceBlock) {
+				scrolls(serviceBlock, getIdLink, e);
+			} else if (e.target.closest("menu") === menu &&
+			(e.target.classList.contains("close-btn") || e.target.closest("a"))) {
+			if (e.target.closest("a") && !e.target.classList.contains("close-btn")) {
+				menu.classList.toggle("active-menu");
+				scrolls(e.target, getIdLink, e);
+			} else {
+				menu.classList.toggle("active-menu");
+				}
+			} else if (e.target.closest("menu") !== menu) {
+				menu.classList.remove("active-menu");
+			}
+		});
 	};
 
 	const getIdLink = (elem) => {
@@ -16,25 +31,7 @@ const menu = () => {
 		return value1;
 	};
 
-	main.addEventListener("click", (e) => {
-		if (e.target.closest(".menu")) {
-			toggleMenu();
-		} else if (e.target === serviceBlock) {
-			scrolls(serviceBlock, getIdLink, e);
-		}
-	});
-
-	menu.addEventListener("click", (e) => {
-		if (e.target.closest("menu") === menu && 
-		(e.target.classList.contains("close-btn") || e.target.closest("li"))) {
-			if (e.target.closest("li")) {
-				toggleMenu();
-				scrolls(e.target, getIdLink, e);
-			} else {
-				toggleMenu();
-			}
-		}
-	});
+	toggleMenu();
 };
 
 export default menu;
